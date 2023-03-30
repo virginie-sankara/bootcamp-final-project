066d8f57c34441bb45b9c94d35768db764a1b87d
“Virginie-Sankara <virginiesankara@gmail.com>
2023-03-30 17:54:49 -0400

    auth0 init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,18 @@
+import { createRoot } from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-lr5q32s3qpq4c4ah.us.auth0.com"
+    clientId="vN72Fx4HQ1b5P0v0TzZsiOcc0UWikGFH"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
