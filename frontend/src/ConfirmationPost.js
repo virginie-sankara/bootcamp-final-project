fb583168f07b277390ebadd8589a269056e7b701
Virginie-Sankara <virginiesankara@gmail.com>
2023-04-11 22:39:23 -0400

    Move

--- frontend/src/ConfirmationPost.js
+++ frontend/src/ConfirmationPost.js
@@ -2,27 +2,28 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+const confirmationMessages = [
+  "Thanks for submitting your answers! You rock!",
+  "Your answers have been received loud and clear. Thanks a bunch!",
+  "High five! Your answers have been submitted successfully.",
+  "Boom! Your answers have been processed. Thanks for taking the time!",
+  "Your answers have been submitted. You're basically a genius.",
+  "Submission successful. You're crushing it, keep it up!",
+  "Your answers have been received. We're pretty impressed, not gonna lie.",
+  "Well done! Your answers have been submitted. You're officially awesome.",
+  "Boom! Your answers have been submitted. Thanks for being awesome!",
+  "Well, well, well, look who submitted their answers like a boss. You did, and we're impressed.",
+  "Yaaas ! Your answers have been submitted flawlessly. Keep slaying.",
+  "Your answers have been successfully transmitted. If only all transmissions were this easy, amirite?",
+  "You just knocked out those answers like a boss. Consider them submitted and consider yourself a boss.",
+];
+
 const ConfirmationPost = () => {
   const navigate = useNavigate();
   const { matchId } = useParams();
   const [matchData, setMatchData] = useState(null);
   // random confirmationMessage
   const [randomMessage, setRandomMessage] = useState("");
-  const confirmationMessages = [
-    "Thanks for submitting your answers! You rock!",
-    "Your answers have been received loud and clear. Thanks a bunch!",
-    "High five! Your answers have been submitted successfully.",
-    "Boom! Your answers have been processed. Thanks for taking the time!",
-    "Your answers have been submitted. You're basically a genius.",
-    "Submission successful. You're crushing it, keep it up!",
-    "Your answers have been received. We're pretty impressed, not gonna lie.",
-    "Well done! Your answers have been submitted. You're officially awesome.",
-    "Boom! Your answers have been submitted. Thanks for being awesome!",
-    "Well, well, well, look who submitted their answers like a boss. You did, and we're impressed.",
-    "Yaaas ! Your answers have been submitted flawlessly. Keep slaying.",
-    "Your answers have been successfully transmitted. If only all transmissions were this easy, amirite?",
-    "You just knocked out those answers like a boss. Consider them submitted and consider yourself a boss.",
-  ];
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * confirmationMessages.length);
